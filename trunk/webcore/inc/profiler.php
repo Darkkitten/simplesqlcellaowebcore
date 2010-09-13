1cde310f1eb0682cca6495836d664aa7cda41b09
iphoneprodigy <iphoneprodigy@ee559dd7-b2ff-6b5a-cb51-558c79971be7>
2010-09-13 05:02:24 +0000

    Updates to show actual Expnsion names from stored bits.

--- trunk/webcore/inc/profiler.php
+++ trunk/webcore/inc/profiler.php
@@ -1,6 +1,70 @@
 <?php
-// My Profile Information
-
+// .:My Profile Information:.
+//
+//
+// BitFields:
+//
+// -Expansions
+// 0 = "Notum Wars"
+// 1 = "Shadow Lands"
+// 2 = "Shadow Lands Pre-Order"
+// 3 = "Alien Invasion"
+// 4 = "Alien Invasion Pre-Order"
+// 5 = "Lost Eden"
+// 6 = "Lost Eden Pre-Order"
+// 7 = "Legacy of Xan"
+// 8 = "Legacy of Xan Pre-Order"
+// 9 = "Mail"
+//10 = "PMV Obsidian Edition"
+// 0=1, 1=2, 2=4, 3=8, 4=16, 5=32, 6=64, 7=128, 8=256, 9=512, 10 = 1024
+// So if you want to give someone all expansions is 1+2+4+8+16+32+64+128+256+512+1024 = 2047
+//
+// -GM Levels
+// 1-100
+// 100=blackmanes statbuffer usable
+$exp="";
+$exps="";
+$Expansion="";
+$exp = $_SESSION['SESS_EXPANSIONS'];
+$exps = (($exp + $exp) -1);
+switch ($exps) {
+	case 1:
+	    $Expansion = "Notum Wars";
+	    break;
+	case 3:
+	    $Expansion = "Shadow Lands";
+	    break;
+	case 7:
+	    $Expansion = "Shadow Lands Pre-Order";
+	    break;
+	case 15:
+	    $Expansion = "Alien Invasion";
+	    break;
+	case 31:
+	    $Expansion = "Alien Invasion Pre-Order";
+	    break;
+	case 63:
+	    $Expansion = "Lost Eden";
+	    break;
+	case 127:
+	    $Expansion = "Lost Eden Pre-Order";
+	    break;
+	case 255:
+	    $Expansion = "Legacy of Xan";
+	    break;
+	case 511:
+	    $Expansion = "Legacy of Xan Pre-Order";
+	    break;
+	case 1023:
+	    $Expansion = "Mail";
+	    break;
+	case 2047:
+	    $Expansion = "PMV Obsidian Edition";
+	    break;
+	default:
+            $Expansion = "Notum Wars";
+            break;
+}
 echo "My User ID: <b>".$_SESSION['SESS_ID']."<br></b>";
 echo "Created on: <b>".$_SESSION['SESS_CREATIONDATE']."<br></b>";
 echo "My Email: <b>".$_SESSION['SESS_EMAIL']."<br></b>";
@@ -8,7 +72,7 @@ echo "My User Name: <b>".$_SESSION['SESS_USER_NAME']."<br></b>";
 echo "My Max Chars: <b>".$_SESSION['SESS_ALLOWED_CHARACTERS']."<br></b>";
 echo "My Gen Flags: <b>".$_SESSION['SESS_FLAGS']."<br></b>";
 echo "My Acnt Flags: <b>".$_SESSION['SESS_ACCOUNTFLAGS']."<br></b>";
-echo "My Expansions: <b>".$_SESSION['SESS_EXPANSIONS']."<br></b>";
+echo "My Expansions: <b>(".$exp."/".$exps.") ".$Expansion."<br></b>";
 echo "My GM LvL: <b>".$_SESSION['SESS_GM']."<br></b>";
 echo "My First Name: <b>".$_SESSION['SESS_FIRST_NAME']."<br></b>";
 echo "My Last Name: <b>".$_SESSION['SESS_LAST_NAME']."<br></b>";
